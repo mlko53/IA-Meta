0e4c16cdd7da69e387d10d6b6b3f3d9eebe38276
Michael <mlko53@stanford.edu>
2018-12-30 14:15:00 -0800

    add constants

---
+++ constants/constants.py
@@ -0,0 +1,8 @@
+"""Define constants to be used throughout the repo."""
+from pathlib import Path
+
+
+# Project Directories
+PROJECT_DIR = Path(__file__).parent.parent
+META_DIR = PROJECT_DIR / "meta"
+CONFIG_DIR = PROJECT_DIR / "preprocess/config"
