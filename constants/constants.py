7d4862a69fbde57bcc586fd2a1aa863239b68356
Michael <mlko53@stanford.edu>
2019-01-02 01:20:48 -0800

    add affective states

--- constants/constants.py
+++ constants/constants.py
@@ -10,6 +10,7 @@ DATA_DIR = PROJECT_DIR / "data"
 # Prepro
 CONFIG_DIR = PROJECT_DIR / "preprocess/config"
 AVI_DIR = CONFIG_DIR / "AVI_items.txt"
+AFFECTIVE_STATES_DIR = CONFIG_DIR / "affective_states.txt"
 META_DIR = CONFIG_DIR / "meta_items.txt"
 DEMO_DIR = CONFIG_DIR / "demo_items.txt"
 SES_DIR = CONFIG_DIR / "SES_items.txt"
--- preprocess/create_base.py
+++ preprocess/create_base.py
@@ -1,6 +1,5 @@
 import json
 import pandas as pd
-import pprint
 
 import config
 from constants import *
@@ -13,8 +12,8 @@ def create_base_df(verbose=False):
 
     if verbose:
         print("Successfully created base dataframe")
-        print("COLUMNS")
-        pprint.pprint(list(df)) 
+        print("{} COLUMNS".format(len(list(df))))
+        print(list(df)) 
 
     return df
 
@@ -48,11 +47,34 @@ def get_AVI_abbreviations(AVI_words, verbose=False):
 
     return AVI_abbreviations
 
-def get_AVI_columns(AVI_words, verbose=False):
+def get_AVI_columns(AVI_words, states=False, verbose=False):
     """Returns AVI columns"""
-    AVI_abbreviations = get_AVI_abbreviations(AVI_words, verbose)
+    if not states:
+        AVI_abbreviations = get_AVI_abbreviations(AVI_words, verbose).values()
+    else:
+        AVI_abbreviations = AVI_words
 
-    return AVI_abbreviations.values()
+    affect_types = ['real', 'ideal']
+    compute_types = ['raw', 'ipsatized']
+    who_computes = ['us', 'them']
+
+    AVI_columns = []
+
+    for compute_type in compute_types:
+        if compute_type == 'raw' and not states:
+            for affect_type in affect_types:
+                for AVI_word in AVI_abbreviations:
+                    col = '.'.join([affect_type[0], AVI_word, compute_type[:3]])
+                    AVI_columns.append(col)
+
+        else:
+            for who in who_computes:
+                for affect_type in affect_types:
+                    for AVI_word in AVI_abbreviations:
+                        col = '.'.join([affect_type[0], AVI_word, compute_type[:3], who])
+                        AVI_columns.append(col)
+
+    return AVI_columns
 
 def get_columns(verbose=False):
     """Return columns of base dataframe"""
@@ -63,15 +85,18 @@ def get_columns(verbose=False):
     demo_items = load_list_from_txt(DEMO_DIR)
     SES_items = load_list_from_txt(SES_DIR)
     AVI_words = load_list_from_txt(AVI_DIR)
+    affective_states = load_list_from_txt(AFFECTIVE_STATES_DIR)
 
     if verbose:
         print("Using {} emotion words".format(len(AVI_words)))
 
-    AVI_columns = get_AVI_columns(AVI_words, verbose=verbose)
+    AVI_columns = get_AVI_columns(AVI_words, states=False, verbose=verbose)
+    states_columns = get_AVI_columns(affective_states, states=True, verbose=verbose)
 
     columns.extend(meta_items)
     columns.extend(demo_items)
     columns.extend(SES_items)
     columns.extend(AVI_columns)
+    columns.extend(states_columns)
 
     return columns
