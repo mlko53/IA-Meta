1a5a0036313111472575779e9b163d68c70e32f5
Michael <mlko53@stanford.edu>
2019-01-07 00:25:54 -0800

    add messy code to compute correlation

---
+++ analysis/compute.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+
+
+def build_aggregator(query_cols):
+    """Building mapping from var name to mean and std"""
+    agg_query = {}
+
+    for var in query_cols:
+        agg_query[var] =\
+            lambda x: [np.nanmean(x), np.nanstd(x, ddof=1), np.count_nonzero(~np.isnan(x))]
+
+    return agg_query
+
+
+def compute_group_mean_sd(df, query_cols):
+    """Compute mean and sd of query cols"""
+
+    agg_query = build_aggregator(query_cols)
+    df = df.groupby(['paper_study', 'ethn']).agg(agg_query).reset_index()
+
+    for col in query_cols:
+        df[[col+'_mean', col+'_sd', col+'_n']] = pd.DataFrame(df[col].values.tolist(), index=df.index)
+        df = df.drop([col], axis=1)
+
+    return df
+
+
+def compute_d(df, query_cols):
+    """Compute difference mean and sd of query cols"""
+
+    df = compute_group_mean_sd(df, query_cols)
+
+    studies_list = set(df['paper_study'].values)
+    df.index = df['paper_study'] + '_' + df['ethn']
+
+    d = {}
+    for col in query_cols:
+        mean = []
+        sd = []
+        n = []
+        effect_size = []
+        for study in studies_list:
+            
+            mean2 = df.loc[study + '_Group2', col + '_mean']
+            mean1 = df.loc[study + '_Group1', col + '_mean']
+            sd2 = df.loc[study + '_Group2', col + '_sd']
+            sd1 = df.loc[study + '_Group1', col + '_sd']
+            n2 = df.loc[study + '_Group2', col + '_n']
+            n1 = df.loc[study + '_Group1', col + '_n']
+
+            study_mean = mean2 - mean1
+
+            study_sd = np.sqrt(((n1 - 1) * sd1 ** 2 + (n2 - 1) * sd2 ** 2) / (n1 + n2 - 2))
+
+            study_d = study_mean / study_sd
+
+            mean.append(study_mean)
+            sd.append(study_sd)
+            n.append(n2 + n1)
+            effect_size.append(study_d)
+
+        d[col+'_diff_mean'] = mean
+        d[col+'_diff_sd'] = sd
+        d['n'] = n
+        d[col+'_d'] = effect_size
+
+    d = pd.DataFrame(d, index = studies_list)
+
+    return d
+
+
+def compute_r(df, var1, var2):
+    """Computes correlation coefficient for each study between var1 and var2"""
+    
+    df = df.groupby('paper_study').corr().iloc[0::2, -1]
+    df = df.reset_index(1).iloc[:, 1:]
+    df = df.rename({var2: '_'.join(['corr', var1, var2])}, axis=1)
+    return df
---
+++ analysis/study_r.py
@@ -0,0 +1,50 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+from utils import load_grouping, filter_studies_with_groups, load_query
+from compute import compute_r
+from constants import *
+
+def get_args():
+    """Preprocessing arguments"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help='print status of current processes')
+    parser.add_argument(
+        "--name",
+        default="debugging",
+        help="the file name to save as")
+    parser.add_argument(
+        "--meta_df_file",
+        required=True,
+        help="the combined dataframe to use")
+    parser.add_argument(
+        "--var1",
+        required=True,
+        help="first variable to correlate")
+    parser.add_argument(
+        "--var2",
+        required=True,
+        help="second variable to correlate")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    if args.verbose:
+        print("Computing r between {} and {}".format(args.var1, args.var2))
+
+    meta_df = pd.read_csv(PREPROCESSED_DIR / args.meta_df_file)
+
+    meta_df['paper_study'] = meta_df['paper'] + '_' + meta_df['study']
+    meta_df = meta_df[['paper_study', args.var1, args.var2]]
+
+    meta_df = compute_r(meta_df, args.var1, args.var2)
+    print(meta_df)
